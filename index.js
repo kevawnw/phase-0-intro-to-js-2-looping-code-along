5b8b0d3e7f7055493cee75696b1c25fe18bf0f45
Kevawn Warren <kevawnw@gmail.com>
2022-02-01 23:21:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, birthday) {
+    let newArray = []
+    for(let c = 0; c < names.length; c++) {
+        newArray.push(`Thank you, ${names[c]}, for the wonderful surprise gift!`)
+    }
+    return newArray;
+
+}
+
+function countDown() {
+    let countdown = 10
+    while(countdown >= 0) {
+        console.log(countdown--);
+    }
+
+
+}
